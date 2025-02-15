d011466f411cab6c4946f799666284da93435da1
Michael Pyle <mpyle101@gmail.com>
2025-02-15 11:32:18 -0700

    Commenting

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { find_workouts, find_records, find_batches } from './workouts'
 
 const main = async () => {
     const user = await find_user('Pyle');
+
+    // Iterator through all the records letting the iterator deal
+    // with getting the next batch of records.
     for await (const rec of find_records(user.id, { limit: 10 })) {
         console.log(rec);
     }
 
     console.log('\n\n');
+
+    // Get records in batches so you can walk through them in chunks
+    // letting the iterator keep track of the offset.
     const iter = find_batches(user.id, { limit: 10 });
     let { value, done } = await iter.next();
     for (const rec of value!) {
